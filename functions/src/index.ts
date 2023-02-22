4d152dfbe1c51be5780f08e5a1bdd3edc7a4fb4d
Nicolas Le Cam <niko.lecam@gmail.com>
2023-02-23 00:13:27 +0100

    chore(functions): Enable more ts strictness
    Signed-off-by: Nicolas Le Cam <niko.lecam@gmail.com>

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,39 @@
 import { logger, region } from "firebase-functions";
 import fetch from "node-fetch";
 
+type BaseEvent<T> = {
+  type: T;
+  timestamp: number;
+  name: string;
+  display_name: string;
+};
+
+export const BattleRoyaleVictoryType = "battleroyale:victory";
+export const MarblesVictoryType = "marbles:victory";
+export const BasketBallVictoryType = "basketball:victory";
+export const DuelVictoryType = "duel:victory";
+
+export type BattleRoyaleVictory = BaseEvent<typeof BattleRoyaleVictoryType>;
+export type MarblesVictory = BaseEvent<typeof MarblesVictoryType>;
+export type BasketBallVictory = BaseEvent<typeof BasketBallVictoryType>;
+export type DuelVictory = BaseEvent<typeof DuelVictoryType>;
+
+export type Event =
+  | BattleRoyaleVictory
+  | MarblesVictory
+  | BasketBallVictory
+  | DuelVictory;
+
 export const onCreateEvent = region("europe-west1")
   .runWith({ secrets: ["GITHUB_TOKEN"] })
   .firestore.document("events/{docId}")
   .onCreate(async (change) => {
-    if (change.data().type !== "battleroyale:victory") return;
-
     const { GITHUB_REPOSITORY, GITHUB_TOKEN, GITHUB_WORKFLOW } = process.env;
+    const data = change.data();
+
+    if ("type" in data === false || data["type"] !== "battleroyale:victory") {
+      return;
+    }
 
     const response = await fetch(
       `https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/workflows/${GITHUB_WORKFLOW}/dispatches`,
